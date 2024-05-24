9b27e767dba28ac63d748e5e10dfb2735124c0d9
AnaFallas <fallas.ana03@gmail.com>
2024-05-24 09:54:28 -0600

    cambio

--- borrador/ControlUnit.sv
+++ borrador/ControlUnit.sv
@@ -16,7 +16,7 @@ module ControlUnit (
 
     always_comb begin
         {AluSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch} = outcome[9:4];
-        Aluop= outcome [3:2];
+        Aluop= ouotcme [3:2];
         Imm = outcome[1:0];
     end
 
@@ -29,7 +29,7 @@ module ControlUnit (
                 3'b110: outcome =10'b001000_01_11;//or
                 endcase
             end
-            7'b0000011: outcome = 10'b111000_10_00;  // load
+            7'b0000011: outcome = 10'b111100_10_00;  // load
             7'b0100011: outcome = 10'b110110_10_01;  //store
             7'b1100011: outcome = 10'b000001_10_10;  //branch
             7'b0000000: outcome = 10'b000000_00_00;
