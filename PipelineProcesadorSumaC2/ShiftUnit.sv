5409739686e01b09d735b69a5b0095f0fba62944
Scrys26 <randysteve@gmail.com>
2024-05-27 01:51:20 -0600

    rezero

--- PipelineProcesadorSumaC2/ShiftUnit.sv
+++ PipelineProcesadorSumaC2/ShiftUnit.sv
@@ -1,6 +1,6 @@
 module ShiftUnit #(parameter n = 64) ( 
-    input logic [n - 1 : 0] input_data;
-    output logic [n - 1 : 0] output_data;
+    input logic [n - 1 : 0] input_data,
+    output logic [n - 1 : 0] output_data
     );
 
     assign output_data = input_data << 1;
