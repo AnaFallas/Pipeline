a823694d7a25be51a0413aea3a66f0ac84980c3c
VeroC25 <vcambronero99@gmail.com>
2024-05-27 14:50:25 -0600

    J

--- PipelineProcesadorSumaC2/InstructionMemory.sv
+++ PipelineProcesadorSumaC2/InstructionMemory.sv
@@ -6,50 +6,50 @@ module InstructionMemory #(parameter size = 64) (
     logic [31:0] Memory [size-1:0];
 
     initial begin
-        /*Memory[0] = 8'hE5;
-        Memory[1] = 8'h03;
-        Memory[2] = 8'h1F;
-        Memory[3] = 8'h8B;
+     
 
-        Memory[4] = 8'hA4;
-        Memory[5] = 8'h00;
-        Memory[6] = 8'h40;
-        Memory[7] = 8'hF8;
-
-        Memory[8] = 8'h86;
-        Memory[9] = 8'h00;
-        Memory[10] = 8'h04;
-        Memory[11] = 8'h8B;
-
-        Memory[12] = 8'hA6;
-        Memory[13] = 8'h10;
-        Memory[14] = 8'h00;
-        Memory[15] = 8'hF8;*/
-
-        Memory[0] = 8'h03;
-        Memory[1] = 8'h21;
-        Memory[2] = 8'hC1;
+        Memory[0] = 8'hB3;
+        Memory[1] = 8'h01;
+        Memory[2] = 8'h41;
         Memory[3] = 8'h00;
 
         Memory[4] = 8'hB3;
-        Memory[5] = 8'h00;
-        Memory[6] = 8'h21;
-        Memory[7] = 8'h00;
+        Memory[5] = 8'h82;
+        Memory[6] = 8'h81;
+        Memory[7] = 8'h40;
+
+        Memory[4] = 8'h83;
+        Memory[5] = 8'ha4;
+        Memory[6] = 8'h42;
+        Memory[7] = 8'h06;
 
         Memory[8] = 8'h23;
-        Memory[9] = 8'h26;
-        Memory[10] = 8'h10;
+        Memory[9] = 8'hA0;
+        Memory[10] = 8'h74;
         Memory[11] = 8'h00;
 
         Memory[12] = 8'h83;
-        Memory[13] = 8'h21;
-        Memory[14] = 8'hC0;
-        Memory[15] = 8'h00;
+        Memory[13] = 8'hA3;
+        Memory[14] = 8'h40;
+        Memory[15] = 8'h01;
 
-        Memory[16] = 8'hE3;
-        Memory[17] = 8'h0A;
-        Memory[18] = 8'h31;
-        Memory[19] = 8'hFE;
+        Memory[16] = 8'hB3;
+        Memory[17] = 8'h01;
+        Memory[18] = 8'h52;
+        Memory[19] = 8'h00;
+
+        Memory[20] = 8'hB3;
+        Memory[21] = 8'h02;
+        Memory[22] = 8'h34;
+        Memory[23] = 8'h40;
+
+        Memory[24] = 8'hE3;
+        Memory[25] = 8'h88;
+        Memory[26] = 8'h50;
+        Memory[27] = 8'hFE;
+
+       
+
 
     
     end
