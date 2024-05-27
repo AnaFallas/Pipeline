f0e3fe1a9ac2579aa9effb2d33ff8fb283f68164
VeroC25 <vcambronero99@gmail.com>
2024-05-26 23:41:12 -0600

    re

--- Pipelineprocesador/comparador.sv
+++ Pipelineprocesador/comparador.sv
@@ -3,7 +3,7 @@ module comparador (
     input logic dato_rs2,
     output logic resultado
 );
-assing comparacion = (dato_rs1==dato_rs2) ? 1'b1 : 1'b0;
+assing resultado = (dato_rs1==dato_rs2) ? 1'b1 : 1'b0;
 
 
 endmodule
